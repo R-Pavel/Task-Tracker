d03bd65b88934e7f23dc60614d5aa042ec85b5a4
pavelrylskii <p.rylskii@slotegrator.space>
2025-10-30 10:35:56 +0700

    feat(task): task dtruct added

--- TaskTracker/main.go
+++ TaskTracker/main.go
@@ -2,4 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+const (
+	ColorReset = "\033[0m"
+	ColorRed = "\033[31m"
+	ColorGreen = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorBlue = "\033[34m"
+	ColorGray = "\033[37m"
+)
+
+type Task struct {
+	ID int `json: "id"`
+	Description string `json: "description"`
+	Status string `json: "status"`
+	CreatedAt time.Time `json: "createdAt"`
+	UpdatedAt time.Time `json: "UpdatedAt"`
+}
+
